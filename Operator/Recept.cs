739f3890864f9c05b9ffe24c719acf6a3b1930bf
JekaLion <djoni-01@mail.ru>
2017-11-29 17:41:33 +0600

    Добавьте файлы проекта.

---
+++ Operator/Recept.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    public class Recept
+    {
+        private string _name;
+        private Material[] _resourceLocation = new Material[9];
+
+        public Recept(string name, Material[] resourceLocaion)
+        {
+            _name = name;
+            _resourceLocation = resourceLocaion;
+        }
+
+        public Recept()
+        {
+            _name = "NULL";
+        }
+
+        public string GetName()
+        {
+            return (_name);
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public Material[] GetResourceLocation()
+        {
+            return _resourceLocation;
+        }
+
+        public void SetResourceLocation(Material[] material)
+        {
+            _resourceLocation = material;
+        }
+
+        public Material GetResourceLocation(int x)
+        {
+            return _resourceLocation[x];
+        }
+
+        public void SetResourceLocation(int x, Material material)
+        {
+            _resourceLocation[x].Copy(material);
+        }
+    }
+}
---
+++ Operator/Workbench.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    public class Workbench
+    {
+        private Recept[] _recept;
+        private Material[] _material;
+
+        public Workbench(Recept[] recept, Material[] material)
+        {
+            _recept = recept;
+            _material = material;
+        }
+
+        public Workbench()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                _recept[i] = new Recept();
+            }
+
+            int z = _recept.Length;
+        }
+
+        public void MainFunction()
+        {
+            // 1,2      1,4     1,6
+            // 3,2      3,4     3,6
+            // 5,2      5,4     5,6
+            string[] worbench = new string[7];
+            worbench[0] = "-------";
+            worbench[1] = "| | | |";
+            worbench[2] = "-------";
+            worbench[3] = "| | | |";
+            worbench[4] = "-------";
+            worbench[5] = "| | | |";
+            worbench[6] = "-------";
+            bool switchForWhile = true;
+            while (switchForWhile)
+            {
+
+            }
+            Console.Clear();
+        }
+
+        public void AddRecept(Recept temp)
+        {
+            Array.Resize(ref _recept, (_recept.Length + 1));
+            _recept[_recept.Length - 1].SetName(temp.GetName());
+            _recept[_recept.Length - 1].SetResourceLocation(temp.GetResourceLocation());
+        }
+
+        public void AddMaterial(Material temp)
+        {
+            Array.Resize(ref _material, (_material.Length + 1));
+            _material[_material.Length - 1].SetName(temp.GetName());
+            _material[_material.Length - 1].SetId(temp.GetId());
+        }
+    }
+}
